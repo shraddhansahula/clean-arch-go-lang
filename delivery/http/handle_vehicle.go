163c8cb4748ae8b8b2c283a0659ebc7235fe4229
Shraddhan Sahula <shraddhan.sahula@gmail.com>
2020-07-31 03:01:49 -0400

    replaced gin with mux and sqllite with postgres

--- delivery/http/handle_vehicle.go
+++ delivery/http/handle_vehicle.go
@@ -1,7 +1,9 @@
 package http
 
 import (
-	"github.com/gin-gonic/gin"
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
 	"net/http"
 	"strconv"
 	"vehicles-service/domain"
@@ -13,31 +15,53 @@ type vehicleHandler struct {
 	vehicleUsecase usecase.VehicleUsecase
 }
 
-func NewVehicleHandler(r *gin.RouterGroup, us usecase.VehicleUsecase) {
+func NewVehicleHandler(r *mux.Router, us usecase.VehicleUsecase) {
 
 	handler := &vehicleHandler{
 		vehicleUsecase: us,
 	}
-	r.GET("/get/:id", handler.getById)
-	r.POST("/add", handler.addVehicle) //curl --header "Content-Type: application/json" --request POST --data '{"brand":"Ferrari","model":"Spider"}' http://localhost:8080/v1/add
-	//mux router
-	//database migrations
+	r.HandleFunc("/get/{id}", handler.getById).Methods("GET")
+	r.HandleFunc("/add", handler.addVehicle).Methods("POST")
+
+	//gin code
+	//r.GET("/get/:id", handler.getById)
+	//r.POST("/add", handler.addVehicle) //curl --header "Content-Type: application/json" --request POST --data '{"brand":"Ferrari","model":"Spider"}' http://localhost:8080/add
 }
 
-func (vh *vehicleHandler) getById(ctx *gin.Context){
-	id, _ := strconv.ParseUint(ctx.Param("id"), 10, 32)
-	vehicles, _ := vh.vehicleUsecase.GetVehicleByID(ctx.Request.Context(), id)
-	ctx.JSON(200, gin.H{"data": vehicles})
+func (vh *vehicleHandler) getById(w http.ResponseWriter, r *http.Request){
+	vars := mux.Vars(r)
+	id, _ := strconv.ParseUint(vars["id"], 10, 32)
+	vehicles, _ := vh.vehicleUsecase.GetVehicleByID(r.Context(), id)
+	payload, _ := json.Marshal(vehicles)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(payload)
+	fmt.Println(vehicles)
 }
 
-func (vh *vehicleHandler) addVehicle(ctx *gin.Context){
+func (vh *vehicleHandler) addVehicle(w http.ResponseWriter, r *http.Request){
 	var input domain.VehicleInput
-	if err := ctx.ShouldBindJSON(&input); err != nil {
-		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		return
-	}
+	_ = json.NewDecoder(r.Body).Decode(&input)
+	fmt.Println(input)
 	vehicle := domain.Vehicle{Brand: input.Brand, Model: input.Model}
-	vh.vehicleUsecase.CreateNewVehicle(ctx, &vehicle)
-	ctx.JSON(http.StatusOK, gin.H{"data": vehicle})
+	vh.vehicleUsecase.CreateNewVehicle(r.Context(), &vehicle)
+	fmt.Println(vehicle)
 }
+
+// gin code
+//func (vh *vehicleHandler) getById(ctx *gin.Context){
+//	id, _ := strconv.ParseUint(ctx.Param("id"), 10, 32)
+//	vehicles, _ := vh.vehicleUsecase.GetVehicleByID(ctx.Request.Context(), id)
+//	ctx.JSON(200, gin.H{"data": vehicles})
+//}
+//
+//func (vh *vehicleHandler) addVehicle(ctx *gin.Context){
+//	var input domain.VehicleInput
+//	if err := ctx.ShouldBindJSON(&input); err != nil {
+//		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+//		return
+//	}
+//	vehicle := domain.Vehicle{Brand: input.Brand, Model: input.Model}
+//	vh.vehicleUsecase.CreateNewVehicle(ctx, &vehicle)
+//	ctx.JSON(http.StatusOK, gin.H{"data": vehicle})
+//}
 
--- main.go
+++ main.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	"fmt"
-	"github.com/gin-gonic/gin"
+	"github.com/gorilla/mux"
+	"os"
+
+	//"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
+	"net/http"
 	_delivery "vehicles-service/delivery/http"
 	"vehicles-service/domain"
 	_repo "vehicles-service/repository/sql"
@@ -12,8 +17,15 @@ import (
 )
 
 func main(){
-	r := gin.Default()
-	db, err := gorm.Open("sqlite3", "test.db")
+	//r := gin.Default()
+	user, _ := os.LookupEnv("POSTGRES_USER")
+	port, _ := os.LookupEnv("POSTGRES_PORT")
+	dbName, _ := os.LookupEnv("POSTGRES_DB")
+	dbPassword, _ := os.LookupEnv("POSTGRES_PASSWORD")
+	host, _ := os.LookupEnv("POSTGRES_HOST")
+	postgresConname := fmt.Sprintf("host=%v port=%v user=%v dbname=%v password=%v sslmode=disable", host, port, user, dbName, dbPassword)
+	r := mux.NewRouter()
+	db, err := gorm.Open("postgres", postgresConname)
 	if err != nil {
 		fmt.Println(err.Error())
 		panic("failed to connect database")
@@ -31,11 +43,13 @@ func main(){
 	repo := _repo.NewSqlVehicleRepository(*db)
 	uc := _usecase.NewVehicleUsecase(repo)
 
-	api := r.Group("/v1")
+	//api := r.Group("/v1")
 
-	_delivery.NewVehicleHandler(api, uc)
+	_delivery.NewVehicleHandler(r, uc)
 
-	r.Run()
-
+	//r.Run()
+	http.ListenAndServe(":8080", r)
 }
 
+//add db migrations
+//add authentication headers
